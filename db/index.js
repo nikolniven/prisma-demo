1d4096951c9ba7d243481cff6f6cc810d8281b9c
Nikol_Niven <ntopalusic@gmail.com>
2025-03-11 14:48:09 +0100

    Add example environment file and comment out author and book creation logic

--- db/index.js
+++ db/index.js
@@ -2,34 +2,34 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-const createAuthorAndBook = async () => {
-  try {
-    // First create the author
-    const author = await prisma.author.create({
-      data: {
-        firstName: 'J. R. R.',
-        lastName: 'Tolkien',
-        bio: 'English writer and philologist',
-      },
-    });
+// const createAuthorAndBook = async () => {
+//   try {
+//     // First create the author
+//     const author = await prisma.author.create({
+//       data: {
+//         firstName: 'J. R. R.',
+//         lastName: 'Tolkien',
+//         bio: 'English writer and philologist',
+//       },
+//     });
 
-    // Then create the book with the author's ID
-    const book = await prisma.book.create({
-      data: {
-        title: 'The Fellowship of the Ring',
-        year: 1954,
-        quantity: 5,
-        genre: ['High fantasy', 'Adventure'],
-        authorId: author.id, // Connect to the author using their ID
-      },
-    });
+//     // Then create the book with the author's ID
+//     const book = await prisma.book.create({
+//       data: {
+//         title: 'The Fellowship of the Ring',
+//         year: 1954,
+//         quantity: 5,
+//         genre: ['High fantasy', 'Adventure'],
+//         authorId: author.id, // Connect to the author using their ID
+//       },
+//     });
 
-    console.log('Success! Created:', book);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-};
+//     console.log('Success! Created:', book);
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// };
 
-createAuthorAndBook();
+// createAuthorAndBook();
 
 module.exports = prisma;
